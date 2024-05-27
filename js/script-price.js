615605362c512b6139b5c70cc0a8ebc39530a4c4
lucanovelli.boolean <luca.novelli.lavoro@gmail.com>
2024-05-27 11:30:06 +0200

    add: added method to check if boxcheck is checked

--- js/script-price.js
+++ js/script-price.js
@@ -17,13 +17,13 @@ form_price.addEventListener("submit", function (event) {
     const work = document.getElementById("work").value;
     const textarea = document.getElementById("exampleFormControlTextarea1").value;
     const code = document.getElementById("code").value;
-    const privacy = document.getElementById("privacy").value;
+    const privacy = document.getElementById("privacy");
 
 
     const flag_check = checkForm(firstname, lastname, email, work, textarea, code, privacy); //FLAG CHE INDICA SE IL CONTROLLO E' ANDATO A BUON FINE
 
     if (flag_check)
-        console.log(firstname + "//" + lastname + "//" + email + "//" + work + "//" + textarea + "//" + code + "" + privacy);
+        console.log(firstname + "//" + lastname + "//" + email + "//" + work + "//" + textarea + "//" + code + "//" + privacy.checked);
     else
         console.log("NON COMPLETI");
 })
@@ -39,7 +39,7 @@ function checkForm(firstname, lastname, email, work, textarea, code, privacy) {
     const regular_expression_email = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
-    if (regular_expression.test(firstname) && regular_expression.test(lastname) && regular_expression_email.test(email) && privacy === "on") {
+    if (regular_expression.test(firstname) && regular_expression.test(lastname) && regular_expression_email.test(email) && privacy.checked) {
         if (work === "Backed Development" || work === "Frontend Development" || work === "Project Analysis")
             return true;
     }
